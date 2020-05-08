4d7defa8462d5125f8dfa7591fcab9ad80ad1135
lukssds <lukssds@gmail.com>
2020-05-08 15:45:44 -0300

    Leitura dos dados

--- src/bar/solucaobar.java
+++ src/bar/solucaobar.java
@@ -1,10 +1,29 @@
 package bar;
 
+import java.util.Locale;
+import java.util.Scanner;
 public class solucaobar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Locale.setDefault(Locale.US);
+		Scanner sc= new Scanner(System.in);
+		
+		char sexo;
+		int cerveja,espeto,refri;
+		
+		
+		
+		System.out.print("Digite o sexo: ");
+		sexo=sc.next().charAt(0);
+		System.out.print("Quantidade de cerveja: ");
+		cerveja=sc.nextInt();
+		System.out.print("Quantidade de espetinhos: ");
+		espeto=sc.nextInt();
+		System.out.print("Quantidade de refrigerantes: ");
+		refri=sc.nextInt();
+		
+		
+		sc.close();
 	}
 
 }
