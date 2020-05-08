306e27691a8d64c5d399efd48619e6f4cbc55296
lukssds <lukssds@gmail.com>
2020-05-08 16:01:30 -0300

    Relatorio gerado

--- src/bar/solucaobar.java
+++ src/bar/solucaobar.java
@@ -10,6 +10,7 @@ public class solucaobar {
 		
 		char sexo;
 		int cerveja,espeto,refri;
+		double consumo, couvert, total, ingresso;
 		
 		
 		
@@ -17,12 +18,52 @@ public class solucaobar {
 		sexo=sc.next().charAt(0);
 		System.out.print("Quantidade de cerveja: ");
 		cerveja=sc.nextInt();
-		System.out.print("Quantidade de espetinhos: ");
-		espeto=sc.nextInt();
 		System.out.print("Quantidade de refrigerantes: ");
 		refri=sc.nextInt();
+		System.out.print("Quantidade de espetinhos: ");
+		espeto=sc.nextInt();
 		
 		
+		if (sexo =='f') { 
+			
+			ingresso=8.0;
+		}
+		
+		else {
+			
+			ingresso=10.0;
+			
+		}
+		
+		consumo=cerveja*5.0 + refri *3.0 + espeto*7.0;
+		
+		if (consumo > 30) {
+			couvert=0.0;
+			
+		}
+		else {
+			couvert=4.0;
+		}
+		
+		total= ingresso + consumo + couvert;
+		
+		System.out.println();
+		System.out.println("RELATÓRIO");
+		System.out.printf("Consumo = R$ %.2f%n", consumo);
+		
+		if (couvert == 0.0) {
+			System.out.println("Isento de Couvert");
+			
+		}
+		
+		else {
+			
+			System.out.printf("Couvert = R$ %.2f%n",couvert);
+		}
+		
+		System.out.printf("Ingresso = R$ %.2f%n", ingresso);
+		System.out.println();
+		System.out.printf("Valor a pagar = R$ %.2f%n",total);
 		sc.close();
 	}
 
